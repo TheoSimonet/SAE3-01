f2794b56aef25c39df47590dd824f04b22972bb8
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2022-12-09 17:05:26 +0100

    Ajout des méthodes update, create et delete sur les projets TER

--- src/Controller/ProjetTERController.php
+++ src/Controller/ProjetTERController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\ProjetTER;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProjetTERController extends AbstractController
 {
@@ -15,4 +18,21 @@ class ProjetTERController extends AbstractController
             'controller_name' => 'ProjetTERController',
         ]);
     }
-}
+
+    #[Route('/projet_ter/{id}/update', name: 'app_projet_ter_update', requirements: ['id' => '\d+'])]
+    public function update(ProjetTER $projet, Request $request, ManagerRegistry $doctrine): Response
+    {
+
+    }
+
+    #[Route('/projet_ter/create', name: 'app_projet_ter_create')]
+    public function create(ManagerRegistry $doctrine, Request $request): Response
+    {
+
+    }
+
+    #[Route('/projet_ter/{id}/delete', name: 'app_projet_ter_delete', requirements: ['id' => '\d+'])]
+    public function delete(ProjetTER $projet, Request $request, ManagerRegistry $doctrine): Response
+    {
+
+    }
