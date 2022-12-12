b4957c91ccb1ab2a7c9eaae83bcdb78dc51d767a
Mathéo OLSEN <olsen.matheo@etudiant.univ-reims.fr>
2022-12-12 18:27:43 +0100

    CSS & JS : OK

---
+++ public/assets/js/search__bar__projet.js
@@ -0,0 +1,20 @@
+document.querySelector("#search__bar").addEventListener('keyup', update)
+
+function update() {
+    console.log("test");
+    let input, filter, section, items, a, i, txtValue;
+    input = document.getElementById('search__bar');
+    filter = input.value.toUpperCase();
+    section = document.querySelector(".projet__bottom");
+    items = section.querySelectorAll(".projet__item");
+
+    for (i = 0; i < items.length; i++) {
+        a = items[i].querySelector(".projet__item__titre");
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            items[i].style.display = "";
+        } else {
+            items[i].style.display = "none";
+        }
+    }
+}
