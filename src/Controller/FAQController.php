fb168f8e3853c7f3ab71076d38134e1fe60c84dc
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2022-12-14 17:53:09 +0100

    Modification du controller afin qu'il puisse passer le résultat de la requête dans l'action

--- src/Controller/FAQController.php
+++ src/Controller/FAQController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FaqRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class FAQController extends AbstractController
 {
     #[Route('/faq', name: 'app_f_a_q')]
-    public function index(): Response
+    public function index(FaqRepository $faqRepository): Response
     {
+
+        $faq = $faqRepository->findBy([], ['id' => 'ASC']);
+
         return $this->render('faq/index.html.twig', [
-            'controller_name' => 'FAQController',
+            'faqs' => $faq,
         ]);
     }
 }
