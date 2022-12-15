c53351ca9924e7201679f9ec096c69ee51a31697
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2022-12-15 08:55:41 +0100

    Création de la forge de données pour les utilisateurs

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<User>
+ *
+ * @method        User|Proxy                     create(array|callable $attributes = [])
+ * @method static User|Proxy                     createOne(array $attributes = [])
+ * @method static User|Proxy                     find(object|array|mixed $criteria)
+ * @method static User|Proxy                     findOrCreate(array $attributes)
+ * @method static User|Proxy                     first(string $sortedField = 'id')
+ * @method static User|Proxy                     last(string $sortedField = 'id')
+ * @method static User|Proxy                     random(array $attributes = [])
+ * @method static User|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static UserRepository|RepositoryProxy repository()
+ * @method static User[]|Proxy[]                 all()
+ * @method static User[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static User[]|Proxy[]                 createSequence(array|callable $sequence)
+ * @method static User[]|Proxy[]                 findBy(array $attributes)
+ * @method static User[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static User[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ */
+final class UserFactory extends ModelFactory
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected function getDefaults(): array
+    {
+        $firstname = self::faker()->firstName();
+        $lastname = self::faker()->lastName();
+        $uniqueNumber = self::faker()->unique()->numerify();
+        $phone = self::faker()->phoneNumber();
+
+        return [
+            'email' => 'user-'.$uniqueNumber.'@example.com',
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'password' => 'test',
+            'phone' => $phone,
+            'roles' => ['ROLE_USER'],
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        return $this
+            ->afterInstantiate(function (User $user) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            })
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return User::class;
+    }
+}
