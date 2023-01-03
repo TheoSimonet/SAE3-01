c580cad93c99954da423b9074498ae4eb7fc40ec
simo0157 <theo.simonet@etudiant.univ-reims.fr>
2023-01-03 17:33:11 +0100

    Ajout des droits d'accéder à la page pour les étudiants, les enseignants et les admins

--- src/Controller/EmploiDuTempsController.php
+++ src/Controller/EmploiDuTempsController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ENSEIGNANT') or is_granted('ROLE_ETUDIANT')")]
 class EmploiDuTempsController extends AbstractController
 {
     #[Route('/emploi_du_temps', name: 'app_emploi_du_temps')]
