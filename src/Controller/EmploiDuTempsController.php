c690347f59ab009ec0b0d1cdd1632e0490bc3e28
simo0157 <theo.simonet@etudiant.univ-reims.fr>
2023-01-03 17:02:30 +0100

    Ajout de la page emploi du temps

---
+++ src/Controller/EmploiDuTempsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmploiDuTempsController extends AbstractController
+{
+    #[Route('/emploi_du_temps', name: 'app_emploi_du_temps')]
+    public function index(): Response
+    {
+        return $this->render('emploi_du_temps/index.html.twig', [
+            'controller_name' => 'EmploiDuTempsController',
+        ]);
+    }
+}
