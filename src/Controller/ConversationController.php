c6ac63c544a1785b65a7a16ea82a2bc4285f61f7
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-01-05 23:21:03 +0100

    Modification de l'action permettant d'insérer le formulaire d'envoie de message en bas de page de la vue

--- src/Controller/ConversationController.php
+++ src/Controller/ConversationController.php
@@ -3,7 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Conversation;
+use App\Entity\Message;
+use App\Form\MessageType;
+use DateTimeImmutable;
+use DateTimeZone;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,12 +24,36 @@ class ConversationController extends AbstractController
         ]);
     }
 
-    #[Route('/conversation/{id}/', name: 'app_conversation_show', requirements: ['id' => '\d+'])]
-    public function show(Conversation $conversation): Response
+    #[Route('/conversation/{id}', name: 'app_conversation_show', requirements: ['id' => '\d+'])]
+    public function show(Conversation $conversation, ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('conversation/show.html.twig', [
-            'conversation' => $conversation,
-        ]);
-    }
+        $message = new Message();
+        $user = $this->getUser();
 
+        $form = $this->createForm(MessageType::class, $message);
+        $form->add('save', SubmitType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message->setContent($form->getData()->getContent());
+            $message->setConversation($conversation);
+            $message->setSendAt(new DateTimeImmutable("now"));
+            $message->setSenderId($user);
+
+            $em = $doctrine->getManager();
+            $em->persist($message);
+            $em->flush();
+
+            return $this->redirectToRoute('app_conversation_show', [
+                'id' => $conversation->getId(),
+            ]);
+
+        }
+
+        return $this->render('conversation/show.html.twig', [
+            'form' => $form->createView(),
+            'conversation' => $conversation
+        ]);
+
+    }
 }
