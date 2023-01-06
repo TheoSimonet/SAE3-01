108d685cee6c102c2901aca1ecde7551ec360431
Cyril MANIL <cyril.manil@etudiant.univ-reims.fr>
2023-01-06 13:48:46 +0100

    Création de 3 users supplémentaires dans le UserFixtures.php

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -11,6 +11,9 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         UserFactory::createOne(['email' => 'root@example.com', 'password' => 'admin', 'roles' => ['ROLE_ADMIN'], 'firstname' => 'admin', 'lastname' => 'admin']);
+        UserFactory::createOne(['email' => 'cyril.manil08@gmail.com', 'password' => 'cyril', 'roles' => ['ROLE_ETUDIANT'], 'firstname' => 'Cyril', 'lastname' => 'Manil', 'phone' => '0782485067']);
+        UserFactory::createOne(['email' => 'matheo.olsen@suntorydev.fr', 'password' => 'suntorydev', 'roles' => ['ROLE_ENTREPRISE'], 'firstname' => 'Mathéo', 'lastname' => 'Olsen']);
+        UserFactory::createOne(['email' => 'philippe.vautrot@univ-reims.fr', 'password' => 'phil', 'roles' => ['ROLE_ENSEIGNANT'], 'firstname' => 'Philippe', 'lastname' => 'Vautrot']);
         UserFactory::createMany(10);
     }
 }
