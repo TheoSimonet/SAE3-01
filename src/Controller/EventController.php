8f8b3424e3295ac2ff3b9bd5d9eb8b6f938ba135
simo0157 <theo.simonet@etudiant.univ-reims.fr>
2023-01-06 16:01:22 +0100

    Début du contrôleur avec la création d'évenements

---
+++ src/Controller/EventController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    #[Route('/event', name: 'app_event')]
+    public function index(): Response
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+
+    public function createEvent(Request $request): Response
+    {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('event_list');
+        }
+
+        return $this->render('event/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
