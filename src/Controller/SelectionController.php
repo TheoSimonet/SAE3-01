682e2e929f88fd73e0f708c39e2cd9bd5db0db29
herm0021 <alexis.hermant@etudiant.univ-reims.fr>
2023-01-08 12:17:24 +0100

    Ajout de l'action permettant de séléctionner un projet TER

--- src/Controller/SelectionController.php
+++ src/Controller/SelectionController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\ProjetTER;
+use App\Entity\Selection;
+use App\Form\SelectionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SelectionController extends AbstractController
 {
-    #[Route('/selection', name: 'app_selection')]
-    public function index(): Response
+    #[Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ETUDIANT')")]
+    #[Route('/selection/create', name: 'app_selection_create')]
+    public function create(ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('selection/index.html.twig', [
-            'controller_name' => 'SelectionController',
+        $em = $doctrine->getManager();
+        $projet = $em->getRepository(ProjetTER::class)->find($request->query->get('idProjet'));
+
+        $selection = new Selection();
+        $form = $this->createForm(SelectionType::class, $selection);
+        $form->add('save', SubmitType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $selection->setIdUser($this->getUser());
+            $selection->setDate(new \DateTimeImmutable('now'));
+
+            $em->persist($selection);
+            $em->flush();
+
+            return $this->redirectToRoute('app_projet_ter', [
+                'id' => $projet->getId(),
+            ]);
+        }
+
+        return $this->render('seletion/create.html.twig', [
+            'projet' => $projet,
+            'form' => $form->createView(),
         ]);
     }
 }
