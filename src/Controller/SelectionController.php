14ae5583e3a33e8f60fe8df00b88f235213b2f07
herm0021 <alexis.hermant@etudiant.univ-reims.fr>
2023-01-08 12:31:25 +0100

    Création d'une séléction d'un projet TER : fonctionnel

--- src/Controller/SelectionController.php
+++ src/Controller/SelectionController.php
@@ -5,8 +5,11 @@ namespace App\Controller;
 use App\Entity\ProjetTER;
 use App\Entity\Selection;
 use App\Form\SelectionType;
+use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -37,7 +40,7 @@ class SelectionController extends AbstractController
             ]);
         }
 
-        return $this->render('seletion/create.html.twig', [
+        return $this->render('selection/create.html.twig', [
             'projet' => $projet,
             'form' => $form->createView(),
         ]);
