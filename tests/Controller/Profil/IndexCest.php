5d4d8fced06b614625132ddab5fb309d158d40d9
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2023-01-09 15:36:13 +0100

    Ajout d'un test : le rôle d'un utilisateur affiché correspond bien à son rôle

--- tests/Controller/Profil/IndexCest.php
+++ tests/Controller/Profil/IndexCest.php
@@ -40,4 +40,52 @@ class IndexCest
         $I->see($user->getEmail(), 'li');
         $I->see($user->getPhone(), 'li');
     }
+
+    public function notWrongUserRole(ControllerTester $I)
+    {
+        // administrateur
+        $admin = UserFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+        $realAdmin = $admin->object();
+
+        $I->amLoggedInAs($realAdmin);
+        $I->amOnPage('/profil');
+        $I->seeResponseCodeIsSuccessful();
+        $I->see('administrateur', 'h2');
+
+        // utilisateur
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER']]);
+        $realUser = $user->object();
+
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/profil');
+        $I->seeResponseCodeIsSuccessful();
+        $I->see('utilisateur', 'h2');
+
+        // étudiant
+        $student = UserFactory::createOne(['roles' => ['ROLE_ETUDIANT']]);
+        $realStudent = $student->object();
+
+        $I->amLoggedInAs($realStudent);
+        $I->amOnPage('/profil');
+        $I->seeResponseCodeIsSuccessful();
+        $I->see('étudiant', 'h2');
+
+        // enseignant
+        $teacher = UserFactory::createOne(['roles' => ['ROLE_ENSEIGNANT']]);
+        $realTeacher = $teacher->object();
+
+        $I->amLoggedInAs($realTeacher);
+        $I->amOnPage('/profil');
+        $I->seeResponseCodeIsSuccessful();
+        $I->see('enseignant', 'h2');
+
+        // entreprise
+        $company = UserFactory::createOne(['roles' => ['ROLE_ENTREPRISE']]);
+        $realCompany = $company->object();
+
+        $I->amLoggedInAs($realCompany);
+        $I->amOnPage('/profil');
+        $I->seeResponseCodeIsSuccessful();
+        $I->see('entreprise', 'h2');
+    }
 }
