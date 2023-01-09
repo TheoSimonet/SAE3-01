866a8e69157505d7097f2fb1269499adb6e77b6f
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2023-01-09 14:29:53 +0100

    Ajout d'un test : l'auteur (membre d'un entreprise) d'un stage peut le modifier

--- tests/Controller/Stage/UpdateCest.php
+++ tests/Controller/Stage/UpdateCest.php
@@ -3,23 +3,26 @@
 
 namespace App\Tests\Controller\Stage;
 
+use App\Factory\StageFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class UpdateCest
 {
-    public function userCannotUpdate(ControllerTester $I)
+    public function authorCanUpdate(ControllerTester $I)
     {
         $user = UserFactory::createOne(['email' => 'teddy.ping@example.com',
             'password' => 'teddy',
-            'roles' => ['ROLE_USER'],
+            'roles' => ['ROLE_ENTREPRISE'],
             'firstname' => 'teddy',
             'lastname' => 'ping']);
         $realUser = $user->object();
 
+        $stage = StageFactory::createOne(['author' => $user]);
+
         $I->amLoggedInAs($realUser);
-        $I->amOnPage('/stage/.');
+        $I->amOnPage('/stage/'.$stage->getId());
         $I->seeResponseCodeIsSuccessful();
-        $I->amOnPage('/stage/./update');
-        $I->canSeePageRedirectsTo('/stage/./update', '/stage/.');
+        $I->amOnPage('/stage/'.$stage->getId().'/update');
     }
 }
