ed64ba77904f3aac4abc89bf252fd219c5391d20
simo0157 <theo.simonet@etudiant.univ-reims.fr>
2023-01-09 14:00:08 +0100

    Ajout du bouton créer sur la page lister les évènements

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -7,9 +7,9 @@ use App\Form\EventType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 
 class EventController extends AbstractController
 {
@@ -23,12 +23,13 @@ class EventController extends AbstractController
             'events' => $events,
         ]);
     }
+
     #[Route('/event/create', name: 'app_event_create')]
-    public function create(Request $request,ManagerRegistry $doctrine): Response
+    public function create(Request $request, ManagerRegistry $doctrine): Response
     {
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
-        $form ->add('Ajouter', SubmitType::class);
+        $form->add('Ajouter', SubmitType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
