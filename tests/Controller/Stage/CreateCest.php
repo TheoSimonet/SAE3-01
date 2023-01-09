6553840670bb0393c9bfb07c78ec4e83ac119782
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2023-01-09 14:57:25 +0100

    Ajout d'un test : un utilisateur, autre qu'un membre d'une entreprise, ne peut pas créer un stage

--- tests/Controller/Stage/CreateCest.php
+++ tests/Controller/Stage/CreateCest.php
@@ -21,4 +21,18 @@ class CreateCest
         $I->amOnPage('/stage/create');
         $I->seeResponseCodeIsSuccessful();
     }
+
+    public function userCannotCreate(ControllerTester $I)
+    {
+        $user = UserFactory::createOne(['email' => 'teddy.ping@example.com',
+            'password' => 'teddy',
+            'roles' => ['ROLE_ETUDIANT'],
+            'firstname' => 'teddy',
+            'lastname' => 'ping']);
+        $realUser = $user->object();
+
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/stage/create');
+        $I->canSeeResponseCodeIs(403);
+    }
 }
