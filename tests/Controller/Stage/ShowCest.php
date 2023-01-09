5ae7f6412d1ac0c0c3e7ce1fc67a4d4ab21b0b79
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2023-01-09 15:15:24 +0100

    Ajout d'un test : les boutons pour modifier/supprimer un stage s'affichent quand l'utilisateur est le créateur du stage

--- tests/Controller/Stage/ShowCest.php
+++ tests/Controller/Stage/ShowCest.php
@@ -25,4 +25,22 @@ class ShowCest
         $I->seeResponseCodeIsSuccessful();
         $I->seeInTitle('Visualisation du stage n°'.$stage->getId());
     }
+
+    public function containsWhenAuthor(ControllerTester $I)
+    {
+        $user = UserFactory::createOne(['email' => 'teddy.ping@example.com',
+            'password' => 'teddy',
+            'roles' => ['ROLE_ENTREPRISE'],
+            'firstname' => 'teddy',
+            'lastname' => 'ping']);
+        $realUser = $user->object();
+
+        $stage = StageFactory::createOne(['author' => $user]);
+
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/stage/'.$stage->getId());
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeElement('#edit');
+        $I->seeElement('#delete');
+    }
 }
