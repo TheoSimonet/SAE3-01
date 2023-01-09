ce3a68beabfe1c55b40a121d1035f39fc2cfb609
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2023-01-09 15:24:57 +0100

    Ajout d'un test : les informations personnelles affichées d'un utilisateur connecté sont bonnes

--- tests/Controller/Profil/IndexCest.php
+++ tests/Controller/Profil/IndexCest.php
@@ -22,4 +22,22 @@ class IndexCest
         $I->seeResponseCodeIsSuccessful();
         $I->seeInTitle('Page de profil de '.$user->getFirstname());
     }
+
+    public function notWrongUserInformations(ControllerTester $I)
+    {
+        $user = UserFactory::createOne(['email' => 'teddy.ping@example.com',
+            'password' => 'teddy',
+            'roles' => ['ROLE_ADMIN'],
+            'firstname' => 'teddy',
+            'lastname' => 'ping']);
+        $realUser = $user->object();
+
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/profil');
+        $I->seeResponseCodeIsSuccessful();
+        $I->see($user->getLastname(), 'li');
+        $I->see($user->getFirstname(), 'li');
+        $I->see($user->getEmail(), 'li');
+        $I->see($user->getPhone(), 'li');
+    }
 }
