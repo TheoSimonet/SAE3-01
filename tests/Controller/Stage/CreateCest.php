4f8b2e882b24ea5f37db48e0ac5f8fe038555112
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2023-01-09 14:55:53 +0100

    Ajout d'un test : un membre d'une entreprise peut créer un stage

---
+++ tests/Controller/Stage/CreateCest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Tests\Controller\Stage;
+
+use App\Factory\UserFactory;
+use App\Tests\Support\ControllerTester;
+
+class CreateCest
+{
+    public function companyCanCreate(ControllerTester $I)
+    {
+        $user = UserFactory::createOne(['email' => 'teddy.ping@example.com',
+            'password' => 'teddy',
+            'roles' => ['ROLE_ENTREPRISE'],
+            'firstname' => 'teddy',
+            'lastname' => 'ping']);
+        $realUser = $user->object();
+
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/stage/create');
+        $I->seeResponseCodeIsSuccessful();
+    }
+}
