2ebfe39ef0f3fcdc6ea60d8e906203f57eda3f68
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2023-01-09 18:04:38 +0100

    Ajout d'un test : un utilisateur (hormis les enseignants) ne peut pas créer un projet TER

--- tests/Controller/ProjetTER/CreateCest.php
+++ tests/Controller/ProjetTER/CreateCest.php
@@ -21,4 +21,18 @@ class CreateCest
         $I->amOnPage('/projet_ter/create');
         $I->seeResponseCodeIsSuccessful();
     }
+
+    public function userCannotCreate(ControllerTester $I)
+    {
+        $user = UserFactory::createOne(['email' => 'teddy.ping@example.com',
+            'password' => 'teddy',
+            'roles' => ['ROLE_ETUDIANT'],
+            'firstname' => 'teddy',
+            'lastname' => 'ping']);
+        $realUser = $user->object();
+
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/projet_ter/create');
+        $I->canSeeResponseCodeIs(403);
+    }
 }
