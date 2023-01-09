b989501f8007f6557c579116cfae3b14dee15542
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2023-01-09 17:50:58 +0100

    Ajout d'un test : les boutons pour modifier/supprimer un projet s'affichent lorsque l'auteur est sur la page

--- tests/Controller/ProjetTER/ShowCest.php
+++ tests/Controller/ProjetTER/ShowCest.php
@@ -25,4 +25,22 @@ class ShowCest
         $I->seeResponseCodeIsSuccessful();
         $I->seeInTitle('Visualisation du projet n°'.$projet->getId());
     }
+
+    public function containsWhenAuthor(ControllerTester $I)
+    {
+        $user = UserFactory::createOne(['email' => 'teddy.ping@example.com',
+            'password' => 'teddy',
+            'roles' => ['ROLE_ENSEIGNANT'],
+            'firstname' => 'teddy',
+            'lastname' => 'ping']);
+        $realUser = $user->object();
+
+        $projet = ProjetTERFactory::createOne(['author' => $user]);
+
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/projet_ter/'.$projet->getId());
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeElement('#edit');
+        $I->seeElement('#delete');
+    }
 }
