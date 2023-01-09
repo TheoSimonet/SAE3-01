5365ba449a71f89ff381563c25201fae23205031
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2023-01-09 13:49:12 +0100

    Ajout de deux premiers tests : contenu du titre de la page des stages quand l'utilisateur est connecté/non connecté

--- tests/Controller/Stage/IndexCest.php
+++ tests/Controller/Stage/IndexCest.php
@@ -4,13 +4,30 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller\Stage;
 
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
 {
-    public function constains(ControllerTester $I)
+    public function constainsWhenAuthenticated(ControllerTester $I)
+    {
+        $user = UserFactory::createOne(['email' => 'root@example.com',
+            'password' => 'admin',
+            'roles' => ['ROLE_ADMIN'],
+            'firstname' => 'admin',
+            'lastname' => 'admin']);
+        $realUser = $user->object();
+
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/stage');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInTitle('Liste des stages');
+    }
+
+    public function constainsWhenNotAuthenticated(ControllerTester $I)
     {
         $I->amOnPage('/stage');
         $I->seeResponseCodeIsSuccessful();
+        $I->canSeePageRedirectsTo('/stage', '/login');
     }
 }
