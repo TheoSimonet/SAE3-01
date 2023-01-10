a14c8f5b7c9034586afd488ab6da799156290a28
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-01-10 20:01:50 +0100

    Ajout de sécurité sur le controller conversation

--- src/Controller/ConversationController.php
+++ src/Controller/ConversationController.php
@@ -9,6 +9,7 @@ use App\Form\MessageType;
 use App\Repository\ConversationRepository;
 use DateTimeImmutable;
 use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,6 +17,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
+#[IsGranted('ROLE_USER')]
 class ConversationController extends AbstractController
 {
     #[Route('/conversation', name: 'app_conversation')]
