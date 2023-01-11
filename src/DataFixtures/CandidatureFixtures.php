6baad88cbd5c03eefd3f46e0f7b4e344faf42a5e
Alexis Hermant <alexis.hermant@etudiant.univ-reims.fr>
2023-01-11 11:28:54 +0100

    Création du Fixtures CandidatureFixtures

---
+++ src/DataFixtures/CandidatureFixtures.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CandidatureFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+
+    }
+}
