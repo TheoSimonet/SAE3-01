a1e2665fd14f8ef4cca47b0402bbb8ab32fab7a6
Cyril Manil <cyril.manil@etudiant.univ-reims.fr>
2023-03-15 18:28:25 +0100

    Ajout d'un test vérifiant qu'un utilisateur membre d'une entreprise peut supprimer un stage qu'il a créé

--- tests/Api/Stage/StageDeleteCest.php
+++ tests/Api/Stage/StageDeleteCest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Api\Stage;
 
+use App\Entity\Stage;
 use App\Factory\StageFactory;
 use App\Factory\UserFactory;
 use App\Tests\Support\ApiTester;
@@ -56,5 +57,31 @@ class StageDeleteCest
         $I->sendDelete('/api/stages/1');
 
         $I->seeResponseCodeIs(403);
+    }
+
+    public function companyMemberUserCanDeleteHisOwnStage(ApiTester $I): void
+    {
+        $userData = [
+            'email' => 'user1@example.com',
+            'firstname' => 'firstname1',
+            'lastname' => 'lastname1',
+            'roles' => ['ROLE_ENTREPRISE'],
+        ];
+        $user = UserFactory::createOne($userData);
+
+        $connected = $user->object();
+
+        $I->amLoggedInAs($connected);
+
+        $stageData = [
+            'titre' => 'Stage en développement web',
+            'description' => 'A la recherche d\'un stagiaire en tant que développeur web',
+            'author' => $user,
+        ];
+        $stage = StageFactory::createOne($stageData);
+
+        $I->sendDelete('/api/stages/'.$stage->getId());
+
+        $I->seeResponseCodeIsSuccessful();
     }
 }
