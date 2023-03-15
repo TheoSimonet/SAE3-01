761e4046bbbb4b99f22a22c32c941a53cdc83447
Cyril Manil <cyril.manil@etudiant.univ-reims.fr>
2023-03-15 08:14:36 +0100

    Ajout des méthodes GET sur la ressource Alternance

--- src/Entity/Alternance.php
+++ src/Entity/Alternance.php
@@ -6,28 +6,37 @@ use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\AlternanceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: AlternanceRepository::class)]
 #[ApiResource(normalizationContext: ['groups' => ['get_Alternance', 'get_User']], order: ['titre' => 'ASC'])]
 #[ApiFilter(OrderFilter::class, properties: ['titre', 'description'], arguments: ['orderParameterName' => 'order'])]
 #[ApiFilter(SearchFilter::class, properties: ['titre' => 'partial', 'description' => 'partial'])]
+#[Get(normalizationContext: ['groups' => ['get_Alternance', 'get_User']])]
+#[GetCollection]
 class Alternance
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['get_Alternance'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['get_Alternance'])]
     private ?string $titre = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['get_Alternance'])]
     private ?string $description = null;
 
     #[ORM\ManyToOne(inversedBy: 'alternances')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['get_Alternance', 'get_User'])]
     private ?User $author = null;
 
     public function getId(): ?int
