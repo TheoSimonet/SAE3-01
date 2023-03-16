936c0b0f4c0088b2fb12b1a349231cd0852635c1
Théo Simonet <theo.simonet@etudiant.univ-reims.fr>
2023-03-16 15:46:31 +0100

    Ajout de la configuration de l'API sur la ressource event

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -2,11 +2,16 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\EventRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EventRepository::class)]
+#[ApiResource(normalizationContext: ['groups' => ['get_Event', 'get_User']], order: ['title' => 'ASC'])]
 class Event
 {
     #[ORM\Id]
