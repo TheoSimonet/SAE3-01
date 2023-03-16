a1586bdd6be0f04c38ca127d009f2dfed2de67d6
Théo Simonet <theo.simonet@etudiant.univ-reims.fr>
2023-03-16 15:54:33 +0100

    Ajout des méthodes get sur la ressource Event

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -9,28 +9,40 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\EventRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+
 
 #[ORM\Entity(repositoryClass: EventRepository::class)]
 #[ApiResource(normalizationContext: ['groups' => ['get_Event', 'get_User']], order: ['title' => 'ASC'])]
 #[ApiFilter(OrderFilter::class, properties: ['title', 'text'], arguments: ['orderParameterName' => 'order'])]
 #[ApiFilter(SearchFilter::class, properties: ['title' => 'partial', 'text' => 'partial'])]
+#[Get(normalizationContext: ['groups' => ['get_Event', 'get_User']])]
+#[GetCollection]
+
 class Event
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['get_Event'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['get_Event'])]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['get_Event'])]
     private ?\DateTimeInterface $startTime = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['get_Event'])]
     private ?\DateTimeInterface $endTime = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['get_Event'])]
     private ?string $text = null;
 
     public function getId(): ?int
