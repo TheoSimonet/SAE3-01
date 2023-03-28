38ded206b1aeb406048ceed715b9e29fabeb3bcc
Cyril Manil <cyril.manil@etudiant.univ-reims.fr>
2023-03-28 10:07:45 +0200

    Ajout de l'action GET sur la ressource Selection

--- src/Entity/Selection.php
+++ src/Entity/Selection.php
@@ -2,27 +2,35 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\Get;
 use App\Repository\SelectionRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: SelectionRepository::class)]
+#[Get(normalizationContext: ['groups' => ['get_Selection', 'get_Projet', 'get_User']],
+    security: "(is_granted('ROLE_ENSEIGNANT') && object.getIdProjet().getAuthor() == user) || (is_granted('ROLE_ETUDIANT') && object.getIdUser().getId() == user.getId())")]
 class Selection
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['get_Selection'])]
     private ?int $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'selections')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['get_Selection', 'get_Projet'])]
     private ?ProjetTER $idProjet = null;
 
     #[ORM\ManyToOne(inversedBy: 'selections')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['get_Selection', 'get_User'])]
     private ?User $idUser = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['get_Selection'])]
     private ?\DateTimeInterface $date = null;
 
     public function getId(): ?int
