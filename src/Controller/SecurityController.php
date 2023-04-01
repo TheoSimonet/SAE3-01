628ec43480f35626cb663fdf837172ad22482651
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-04-01 03:55:52 +0200

    Ajout de deux routes permettant la connexion/déconnexion d'un utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
 
 class SecurityController extends AbstractController
 {
@@ -27,6 +31,26 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/api/login', name: 'api_login', methods: ['POST'])]
+    public function APILogin(JWTTokenManagerInterface $jwtManager): JsonResponse
+    {
+        $user = $this->getUser();
+
+        // generate token
+        $token = $jwtManager->create($user);
+
+        // return token as JSON response
+        return $this->json([
+            'token' => $token,
+        ]);
+    }
+
+    #[Route(path: '/api/logout', name: 'api_logout', methods: ['POST'])]
+    public function APILogout(): Response
+    {
+        return new Response('', Response::HTTP_NO_CONTENT);
     }
 }
